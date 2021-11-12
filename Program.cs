4dd5a13d009d215432df04684a14a0add610dc78
Andrewkovsky Pedrosa Alves <90865422+DeKovsky@users.noreply.github.com>
2021-11-12 09:44:19 -0300

    Add files via upload
    adicionado todos os arquivos da atividade 01 da uc 01 do SENAc

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_2___Atividade_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja Bem Vindo ao meu Projeto SENAC");
+            Console.WriteLine("Qual o seu nome?");
+        }
+    }
+}
+
